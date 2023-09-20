5ef49aa9fcbae8b3d43232354b30ee6c4e1b1e23
NEXUSHARE <harecharan321@gmail.com>
2023-09-20 16:53:09 +0530

    .

--- 92.py
+++ 92.py
@@ -4,13 +4,13 @@
 
 
 # Definition for singly-linked list.
-# class ListNode:
-#     def __init__(self, val=0, next=None):
-#         self.val = val
-#         self.next = next
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
 
 class Solution:
-    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+    def reverseBetween(self, head, left, right):
         if not head or left == right:
             return head
         
